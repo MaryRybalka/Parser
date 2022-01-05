99e689fc3969eda33d89a91df07c8ed29ac204ff
masha <rybalkamasha64@gmail.com>
2022-01-06 01:04:17 +1000

    add sigma

--- Parser.cs
+++ Parser.cs
@@ -9,9 +9,17 @@ enum Precedence : byte
     EQUAL = 1,
     OROR = 2,
     ANDAND = 3,
-    LESS = 7, MORE = 7, LESSOREQUAL= 7, MOREOREQUAL= 7, TWO_EQUAL = 7, NOTEQUAL = 7,
-    PLUS = 10, MINUS = 10,
-    STAR = 20, SLASH = 20, PERCENT = 20,
+    LESS = 7,
+    MORE = 7,
+    LESSOREQUAL = 7,
+    MOREOREQUAL = 7,
+    TWO_EQUAL = 7,
+    NOTEQUAL = 7,
+    PLUS = 10,
+    MINUS = 10,
+    STAR = 20,
+    SLASH = 20,
+    PERCENT = 20,
 }
 
 public class Parser
@@ -62,61 +70,82 @@ public class Parser
         DecimalSymbol,
         FloatPart,
         Exp,
-        ESign,
         Sign
     };
-    
-    private readonly string[] _regexes =
-        {
-            "\\n",
-            "\\s",
-            "[a-zA-Z]",
-            "[0-9]",
-            "\"",
-            "\\\\",
-            "\\.",
-            "\\/",
-            "\\*",
-            "\\+",
-            "\\-",
-            "\\%",
-            "\\!",
-            "\\=",
-            "\\(",
-            "\\)",
-            "\\{",
-            "\\}",
-            "\\,",
-            "\\;",
-            "\\&",
-            "\\|",
-            "\\>",
-            "\\<",
-            "."
-        };
 
-        private readonly SortedSet<string> _keywords = new SortedSet<string>
-        {
-            "var",
-            "let",
-            "for",
-            "while",
-            "if",
-            "in",
-            "else"
-        };
-        private readonly SortedSet<string> _reservedNames = new SortedSet<string>
-        { 
-            "string",
-            "substring",
-            "character",
-            "bool",
-            "array",
-            "int",
-            "double",
-            "float",
-            "true",
-            "false",
-            "nil"
-        };
+    private readonly string[] sigma =
+    {
+        "[a-zA-Z]",
+        "[0-9]",
+        "(",
+        ")",
+        "{",
+        "}",
+        "=",
+        "/",
+        "*",
+        "+",
+        "-",
+        "%",
+        "!",
+        "?",
+        ">",
+        "<",
+        "."
+    };
+
+    private readonly string[] _regexes =
+    {
+        "\\n",
+        "\\s",
+        "[a-zA-Z]",
+        "[0-9]",
+        "\"",
+        "\\\\",
+        "\\.",
+        "\\/",
+        "\\*",
+        "\\+",
+        "\\-",
+        "\\%",
+        "\\!",
+        "\\=",
+        "\\(",
+        "\\)",
+        "\\{",
+        "\\}",
+        "\\,",
+        "\\;",
+        "\\&",
+        "\\|",
+        "\\>",
+        "\\<",
+        "."
+    };
+
+    private readonly SortedSet<string> _keywords = new SortedSet<string>
+    {
+        "var",
+        "let",
+        "for",
+        "while",
+        "if",
+        "in",
+        "else"
+    };
+
+    private readonly SortedSet<string> _reservedNames = new SortedSet<string>
+    {
+        "string",
+        "substring",
+        "character",
+        "bool",
+        "array",
+        "int",
+        "double",
+        "float",
+        "true",
+        "false",
+        "nil"
+    };
 }
